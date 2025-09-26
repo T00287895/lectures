df494f170be209d18d74f9579a8e1b15f7f39b2e
t00287895-hue <t00287895@gmail.com>
2025-09-26 10:47:36 +0100

    init

---
+++ Structure programming/25-09-25.py
@@ -0,0 +1,12 @@
+import sys
+
+speed = float(input("Speed?"))
+distance =  float(input("Distance?"))
+
+print("time", distance / speed, "km", sep=" ")
+#
+# integer = 10
+# print("The size of the integer variable is:",sys.getsizeof(integer), "bytes.")
+#
+# _float = 10
+# print("The size of the _float variable is:",sys.getsizeof(_float), "bytes.")
