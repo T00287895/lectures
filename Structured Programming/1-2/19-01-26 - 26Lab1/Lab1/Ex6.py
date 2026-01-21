81ae3f67765aba1340aa3df2ef1d38be196a15c7
T00287162 <t00287162@mymtu.ie>
2026-01-21 11:49:03 +0000

    Fix palindrome check logic and update success message in Ex6.py

--- Structured Programming/1-2/19-01-26 - 26Lab1/Lab1/Ex6.py
+++ Structured Programming/1-2/19-01-26 - 26Lab1/Lab1/Ex6.py
@@ -6,8 +6,8 @@ while True:
         print("Word is too long! Try again")
     elif any(char.isdigit() for char in word):
         print("Word cannot contain digits! Try again")
-    elif word[0:8] != word[]:
+    elif word[0] != word[-1] and word[1] != word[-2] and word[2] != word[-3] and word[3] != word[-4]:
         print("The word is not palindrome! Try again")
     else:
         break
-print("The word you have entered is palindrome")
+print("The word you have entered is palindrome!")
