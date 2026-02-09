ab689e30e4ed09d1b4d79088d40b7ac16d187ea3
T00287162 <t00287162@mymtu.ie>
2026-02-09 10:34:59 +0000

    hello

---
+++ Structured Programming/1-2/Lab 7/Ex 2.py
@@ -0,0 +1,20 @@
+from tkinter import simpledialog, messagebox
+
+def numberOfDaysIn():
+    
+    if month % 2 == 0:
+        days = 30
+    if month == 2:
+        days = 28
+    elif month % 2 != 0:
+        days = 32
+    return days
+
+while True:
+    month = simpledialog.askinteger("Input", "Please enter the number of a month:")
+
+    if month > 12 or month < 1:
+        messagebox.showinfo("Days in Month", "Invalid - you must enter a month number between 1 and 12")
+    else:
+        messagebox.showinfo("Days in Month", f"There are {numberOfDaysIn()} in this month")
+        break
